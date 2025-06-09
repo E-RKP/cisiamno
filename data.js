50a491c0ff9cd3ba4a2c56d56fa77d7dcdf8be04
E-RKP <edobackup111@gmail.com>
2025-06-09 16:08:57 +0200

    version 4

--- data.js
+++ data.js
@@ -35,3 +35,92 @@ utente1.deposita(100)
 utente1.preleva(100)
 utente1.deposita(4000)
 console.log(utente1)
+
+
+class coglione extends ContoBancario{
+  constructor (name, surname, saldo, siono){
+    super(name, surname, saldo)
+
+    if (siono === true){
+      this.coglione = 'lo è'
+    }else{
+      this.coglione = 'non lo è'
+    }
+    
+  }
+
+}
+
+let utente2 = new coglione ('mario', 'rossi', 0, false)
+console.log(utente2)
+
+
+let persona = {
+  
+  nome: 'Mario',
+  cognome: 'rossi',
+  età: 100,
+  saluta: function(){
+    return `Ciao mi chiamo ${this.nome} ${this.cognome} e ho ${this.età} anni`
+  }
+}
+
+console.log(persona.saluta())
+function aggiungiEtà(oggetto){
+  oggetto.età = oggetto.età+1
+}
+aggiungiEtà(persona);
+console.log(persona.età)
+
+let carello = {
+  prodotti: [
+
+    {
+      nome: 'yogurt',
+      quantità: 10,
+      prezzo: 0.99
+    },
+
+    {
+      nome: 'salame',
+      quantità: 1,
+      prezzo: 5.99
+    },
+
+    {
+      nome: 'maionese',
+      quantità: 24,
+      prezzo: 1.69
+    },
+
+
+    {
+      nome: 'limone',
+      quantità: 100,
+      prezzo: 0.35
+    },
+
+
+    {
+      nome: 'acqua',
+      quantità: 314,
+      prezzo: 0.45
+    },
+  ],
+
+  aggiungiProdotto: function(prodotto){
+    for (i=0; i<carello.prodotti.length;i++){
+      if (carello.prodotti[i].nome === prodotto){
+        return;
+      }
+    }
+    carello.prodotti.push({
+      nome: prodotto,
+      quantità: 1,
+      prezzo: 'non ancora disponibile'
+    })
+  }
+}
+
+carello.aggiungiProdotto('mozzarella')
+console.log(carello.prodotti)
